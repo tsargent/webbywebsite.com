eab1b5fa0ada387dffa3369add115dca4369b726
Tyler Sargent <tsargent@gmail.com>
2020-02-15 16:27:18 -0500

    refactoring

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,46 +1,15 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const result = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          sort: { fields: [frontmatter___date], order: DESC }
-          limit: 1000
-        ) {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
-          }
-        }
-      }
-    `
-  )
-
-  if (result.errors) {
-    throw result.errors
-  }
-
-  // Create blog posts pages.
-  const posts = result.data.allMarkdownRemark.edges
-
+const createBlogPosts = ({ data, actions }) => {
+  const posts = data.edges;
   posts.forEach((post, index) => {
     const previous = index === posts.length - 1 ? null : posts[index + 1].node
     const next = index === 0 ? null : posts[index - 1].node
 
-    createPage({
+    actions.createPage({
       path: post.node.fields.slug,
-      component: blogPost,
+      component: path.resolve(`./src/templates/blog-post.js`),
       context: {
         slug: post.node.fields.slug,
         previous,
@@ -48,6 +17,39 @@ exports.createPages = async ({ graphql, actions }) => {
       },
     })
   })
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+
+  const {data, errors} = await graphql(`
+      query {
+        blog: allMarkdownRemark(
+            sort: { fields: [frontmatter___date], order: DESC }
+            limit: 1000
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  title
+                }
+              }
+            }
+          }
+        }
+    `
+  )
+
+  if (errors) {
+    throw errors
+  }
+
+  createBlogPosts({
+    data: data.blog,
+    actions
+  });
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
